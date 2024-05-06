7246e7680a056f096fe0a1ba548728bb30fb4471
Hassan Rasool <hassanrasool1057@gmail.com>
2024-05-06 22:11:10 +0500

    update requirements

--- main.py
+++ main.py
@@ -43,6 +43,14 @@ def flow():
     user = 'johnricky4549@gmail.com'
     password = 'Jamnager4549$'
 
+    # options = Options()
+    # options.binary_location = '/opt/headless-chromium'
+    # options.add_argument('--headless')
+    # options.add_argument ('--no-sandbox')
+    # options.add_argument ('--single-process')
+    # options.add_argument('--disable-dev-shm-usage')
+    # driver = webdriver.Chrome('/opt/chromedriver', chrome_options=options)
+
     driver = webdriver.Chrome()
     # Maximize the browser window
     driver.maximize_window()
